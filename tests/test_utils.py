9c5f68748a4c0f0699dd9a7f873a2808b7b8f518
philip-papasavvas <39246705+philip-papasavvas@users.noreply.github.com>
2020-09-16 14:48:25 +0100

    added unit tests for utils

---
+++ tests/test_utils.py
@@ -0,0 +1,110 @@
+# Created on 16 Sep 2020 for Unit tests
+
+import unittest
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+
+from utils import to_array
+
+
+class MyTestCase(unittest.TestCase):
+    def test_to_array__list_ints(self):
+        # lists - ints: check get same results as using np.array(list)
+        list_ints = [1, 2, 3]
+        x, = to_array(list_ints)
+        y = np.array(list_ints)
+        np.testing.assert_array_equal(x, y)
+
+    def test_to_array__list_floats(self):
+        # lists - floats: check get same results as using np.array(list)
+
+        list_floats = [1.0, 2 / 100, 3.14]
+        x, = to_array(list_floats)
+        y = np.array(list_floats)
+        np.testing.assert_array_equal(x, y)
+
+    def test_to_array__list_str(self):
+        # list - str
+        list_str = ["A", "B", "C"]
+        x, = to_array(list_str)
+        y = np.array(list_str)
+        np.testing.assert_array_equal(x, y)
+
+    def test_to_array__list_mixed(self):
+        # lists
+        list_ints = [1, 2, 3]
+        list_floats = [1.0, 2 / 100, 3.14]
+        list_str = ["A", "B", "C"]
+        x = [i for i in to_array(list_ints, list_floats, list_str)]
+        y = [np.array(i) for i in [list_ints, list_floats, list_str]]
+        np.testing.assert_array_equal(x, y)
+
+    def test_to_array__tuple(self):
+        x, = to_array((1, 2, 3, 4))
+        y = np.array((1, 2, 3, 4))
+        np.testing.assert_array_equal(x, y)
+
+    def test_to_array__np_array(self):
+        y = np.array([1, 2, 3])
+        x, = to_array(y)
+        np.testing.assert_array_equal(x, y)
+
+    def test_to_array__mixed_single_values(self):
+        # single values: ints, floats, str, bool
+        listed_mixed_vals = [1, 2.0, '3', True]
+        # - unpack the list
+        z = [i for i in to_array(*listed_mixed_vals)]
+        y = [np.array([i]) for i in listed_mixed_vals]
+        np.testing.assert_array_equal(z, y)
+
+    def test_to_array__single_value_np_dtypes(self):
+        # single values: np dtypes
+        list_int = [getattr(np, f'int{i}')(i) for i in [8, 16, 32, 64]]
+        list_float = [getattr(np, f'float{i}')(i) for i in [16, 32, 64]]
+        list_bool = [getattr(np, i)(1) for i in ['bool', 'bool8', 'bool_']]
+        list_np_datetime = [np.datetime64('2020-12-03')]
+        for ll in [list_int, list_float, list_bool, list_np_datetime]:
+            z = [i for i in to_array(*ll)]
+            y = [np.array([i]) for i in ll]
+            np.testing.assert_array_equal(z, y)
+
+    def test_to_array__none(self):
+        x, = to_array(None)
+        np.testing.assert_array_equal(x, np.array([]))
+
+    def test_to_array__datetime(self):
+        x, = to_array(datetime(2020, 12, 3))
+        y = np.array(['2020-12-03'], dtype='datetime64[D]')
+        np.testing.assert_array_equal(x, y)
+
+    def test_to_array__pandas(self):
+        # pandas
+        # Series: list to Series and get values same as np.array from list
+        # - NOTE: skiped listed_mixed_vals
+
+        list_ints = [1, 2, 3]
+        list_floats = [1.0, 2 / 100, 3.14]
+        list_str = ["A", "B", "C"]
+        list_int = [getattr(np, f'int{i}')(i) for i in [8, 16, 32, 64]]
+        list_float = [getattr(np, f'float{i}')(i) for i in [16, 32, 64]]
+        list_bool = [getattr(np, i)(1) for i in ['bool', 'bool8', 'bool_']]
+        list_np_datetime = [np.datetime64('2020-12-03')]
+
+        lls = [list_ints, list_floats, list_str, list_int, list_float, list_bool, list_np_datetime]
+        pds = [pd.Series(i) for i in lls]
+        z = [i for i in to_array(*pds)]
+        y = [np.array(i) for i in lls]
+        for j in range(len(z)):
+            np.testing.assert_array_equal(z[j], y[j])
+
+        # Index
+        pds = [pd.Index(i) for i in lls]
+        z = [i for i in to_array(*pds)]
+        for j in range(len(z)):
+            np.testing.assert_array_equal(z[j], y[j])
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ utils.py
@@ -0,0 +1,146 @@
+"""
+Created on: 16 Sep 2020
+Created by: Philip.P_adm
+
+Generic Utils functions
+"""
+import datetime as dt
+import os
+import re
+from typing import Union
+
+import numpy as np
+import pandas as pd
+
+
+def to_array(*args: Union[np.ndarray, list, tuple, pd.Series, np.datetime64, dt.datetime]):
+    """Turning x into np.ndarray
+
+    Yields:
+        :class:'np.ndarray'
+
+    Raises:
+        ValueError if x is not in the listed type
+
+    Example
+    >>> import numpy as np
+    >>> x,y,z = to_array(2,["a","b"],None)
+    >>> date_array, =  to_array(np.datetime64("2019-01-01"))
+    """
+
+    for x in args:
+        if isinstance(x, dt.date):
+            yield np.array([x.strftime('%Y-%m-%d')], dtype='datetime64[D]')
+            # if already an array yield as is
+        elif isinstance(x, np.ndarray):
+            yield x
+        elif isinstance(x, (list, tuple)):
+            yield np.array(x)
+        elif isinstance(x, (pd.Series, pd.core.indexes.base.Index, pd.core.series.Series)):
+            yield x.values
+        elif isinstance(x, (int, float, str, bool, np.bool_)):
+            yield np.array([x], dtype=type(x))
+        # np.int{#}
+        elif isinstance(x, (np.int8, np.int16, np.int32, np.int64)):
+            yield np.array([x], dtype=type(x))
+        # np.float{#}
+        elif isinstance(x, (np.float16, np.float32, np.float64)):
+            yield np.array([x], dtype=type(x))
+        # np.bool*
+        elif isinstance(x, (np.bool, np.bool_, np.bool8)):
+            yield np.array([x], dtype=type(x))
+        # np.datetime64
+        elif isinstance(x, np.datetime64):
+            yield np.array([x], "datetime64[D]")
+        elif x is None:
+            yield np.array([])
+        else:
+            from warnings import warn
+            warn(f"Data type {type(x)} is not configured in to_array.")
+            yield np.array([x], dtype=object)
+
+
+def find(folder_path, pattern='.*', full_path=False, expect_one=True):
+    """
+    To find path(s) of file(s), especially useful for searching the same file pattern in multiple
+    folders
+
+    Args: folder_path (str, list, np.ndarray): Folder path(s). If multiple, it will be searched
+    in its order pattern (str, list/tuple): regex pattern. Use list/tuple if you need multiple
+    conditions full_path (bool, default False): if the full path of the files are needed
+    expect_one (bool, default True): True will raise AssertionError if more than one file is found
+
+    Returns:
+        str: If one file is found
+        list of str: If multiple files are found
+
+    Note:
+        This function can only handle same search pattern for every folderPath, and will return the first one it finds \
+        if there are multiple folderPath. If it cannot find any, it will raise exceptions about the first folderPath
+
+    Raises:
+        FileNotFoundError: If folderPath(s) or pattern(s) does not match any findings
+        FileExistsError: If more than one file is found when expectOne = True
+    """
+    if isinstance(folder_path, str):
+        folder_path, = to_array(folder_path)
+
+    for i, path in enumerate(folder_path):
+
+        try:
+            list_of_files = os.listdir(path=path)
+        except (FileNotFoundError, OSError) as err:
+            if i < len(folder_path) - 1:
+                print(err.args[-1] + ' for "%s",... trying next' % path)
+                continue  # go for next folderPath
+            # out of luck
+            err.args = (err.args[0], err.args[1] + ': %s' % path)  # raise with first folderPath
+            raise
+
+        if isinstance(pattern, (list, tuple)):
+            n = len(pattern)
+            # multi condition pattern matching
+            ipattern = '|'.join(pattern)
+            # strict matching of all required patters
+            files = [f for f in list_of_files if
+                     np.unique(re.findall(ipattern, f, re.IGNORECASE)).size == n]
+        else:
+            files = [f for f in list_of_files if re.findall(pattern, f, re.IGNORECASE)]
+
+        try:
+            if len(files) == 0:
+                # remove some special characters before raising error
+                if isinstance(pattern, str):
+                    pattern = re.sub('[^A-Za-z0-9_.-]+', '', pattern)
+                else:
+                    pattern = [re.sub('[^A-Za-z0-9_.-]+', '', pat) for pat in pattern]
+                raise FileNotFoundError(
+                    '%s exists but no file names with pattern: %s' % (path, pattern))
+            elif (len(files) > 1) and expect_one:
+                raise FileExistsError('%s exists but %s files found' % (path, len(files)))
+
+        except (FileNotFoundError, FileExistsError) as err:
+            if i < len(folder_path) - 1:
+                print(err.args[0] + ',... trying next')
+                continue  # go for next folderPath
+            # out of luck, raise with first folderPath
+            err.args = (
+                re.sub(path.replace('\\', '/'), folder_path[0],
+                       err.args[0]),)  # re.sub doesn't like double backslashes
+            raise
+
+        break  # stop loop when we got the files we wanted
+
+    if full_path:
+        if path[-1] == '/':
+            path = path[:-1]
+        files = ['/'.join((path, f)) for f in files]
+
+    if len(files) == 1 and expect_one:
+        files = files[0]
+
+    return files
+
+
+if __name__ == '__main__':
+    pass
